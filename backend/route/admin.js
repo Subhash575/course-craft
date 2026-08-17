a5d03c6209c801d5c62f02ddaa7d60fd2cc73c72
Subhash575 <subhash100393@gmail.com>
2026-08-17 21:07:18 +0530

    implemented the admin-courese delete endpoint

--- backend/route/admin.js
+++ backend/route/admin.js
@@ -239,7 +239,30 @@ router.put("/course", adminMiddleware, async (req, res) => {
 });
 
 // delete course
-router.delete("/course/:id", (req, res) => {});
+router.delete("/course/:id", adminMiddleware, async (req, res) => {
+  try {
+    const adminId = req.userId;
+    const courseId = req.params.id;
+    const course = await CourseModel.findById(courseId);
+
+    // checking "course existed or not".
+    if (!course) {
+      return res.status(404).json({ message: "Course not found" });
+    }
+
+    // checking the owner of the course
+    if (adminId.toString() !== course.creatorId.toString()) {
+      return res.status(403).json({ message: "Unauthorized access" });
+    }
+
+    // Important point is "filter parameter" name need to match with. Model schema name for
+    // filtering.
+    const delCourse = await CourseModel.deleteOne({ _id: courseId });
+    res.status(200).json({ message: delCourse });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
 
 // admin get all of their courses in bulk
 router.get("/course/bulk", (req, res) => {});
